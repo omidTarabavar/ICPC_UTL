4c981e085f6ae90c76f08bc8b6cf8400bc0208df
Omid Tarabavar <omidtarab1300@gmail.com>
2025-02-03 15:55:11 +0330

    Add 5

---
+++ Basics/5_2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cin >> a >> b;
+	if (a % b == 0 || b % a == 0)
+		cout << "Multiples" << endl;
+	else
+		cout << "No Multiples" << endl;
+}
